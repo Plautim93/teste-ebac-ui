20171de5da2e0a1ed9581a12eda159b0b1a4d93c
Samuel Plauto <samuelplautobn@gmail.com>
2024-09-17 14:43:06 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('plauto.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, plauto.teste (não é plauto.teste? Sair)')
+    })
+})
