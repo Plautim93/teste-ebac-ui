3b2dd6b2efe3fbef59435a68fba84727d0559e1e
Samuel Plauto <samuelplautobn@gmail.com>
2024-09-18 10:37:06 -0300

    selecionando listas

---
+++ cypress/e2e/3-loja-ebac/produto.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Apollo Running Short')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+    
+});
